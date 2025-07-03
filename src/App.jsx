1289f94db1a9df4565f6fe0877c570e324bc84fd
rimi-w <whi1230@naver.com>
2025-07-04 00:46:09 +0900

    💄 [React 6일차 과제]
    - 라우팅으로 페이지 연결하기
    
    - Outlet 중첩구조 사용하기

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Users from './pages/admin__users/Users';
 import UserDetail from './pages/admin__users/UserDetail';
 import Settings from './pages/admin__settings/Settings';
 import NotFound from './pages/admin__notfound/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   //1. React Router의 `BrowserRouter`를 설정해야 합니다.
@@ -16,7 +17,21 @@ function App() {
   //6. /admin/settings에서 Settings를 렌더링 해야 합니다.
   //7. /admin/<잘못된 페이지명>에서 NotFound를 렌더링 해야 합니다.
 
-  return;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='/admin' element={<Dashboard />}></Route>
+          <Route path='/admin/users' element={<Users />}></Route>
+          <Route path='/admin/users/:id' element={<UserDetail />}></Route>
+          <Route path='/admin/settings' element={<Settings />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+
+    </>
+  );
 }
 
 export default App;
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <Link className="home" to={'/admin'}>
-      <h1>어드민 페이지 이동</h1>
-    </Link>
+    <>
+      <Link className="home" to={'/admin'}>
+        <h1>어드민 페이지 이동</h1>
+      </Link>
+    </>
   );
 };
 
